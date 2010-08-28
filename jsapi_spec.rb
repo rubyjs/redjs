e2e534ca718e38361fca431dae545ed7cb689679
Charles Lowell <cowboyd@thefrontside.net>
2010-08-28 17:07:00 -0500

    fix whitespace.

--- jsapi_spec.rb
+++ jsapi_spec.rb
@@ -12,36 +12,36 @@ describe "Ruby Javascript API" do
     it "can evaluate some javascript" do
       @cxt.eval("5 + 3").should == 8
     end
-  
+
     it "can pass back null to ruby" do
       @cxt.eval("null").should be_nil
     end
-  
+
     it "can pass back undefined to ruby" do
       @cxt.eval("this.undefined").should be_nil
     end
-  
+
     it "can pass the empty string back to ruby" do
       @cxt.eval("''").should == ""
     end
-  
+
     it "can pass doubles back to ruby" do
       @cxt.eval("2.5").should == 2.5
     end
-  
+
     it "can pass fixed numbers back to ruby" do
       @cxt.eval("1").should == 1
     end
-  
+
     it "can pass boolean values back to ruby" do
       @cxt.eval("true").should be(true)
       @cxt.eval("false").should be(false)
     end  
-  
+
     it "treats nil and the empty string as the same thing when it comes to eval" do
       @cxt.eval(nil).should == Context.eval('')
     end
-  
+
     it "can pass back strings to ruby" do
       @cxt['foo'] = "Hello World"
       @cxt.eval("foo").should == "Hello World"
@@ -51,7 +51,7 @@ describe "Ruby Javascript API" do
       lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis faucibus, diam vel pellentesque aliquet, nisl sapien molestie eros, vitae vehicula libero massa vel neque. Phasellus tempor pharetra ipsum vel venenatis. Quisque vitae nisl vitae quam mattis pellentesque et in sapien. Sed at lectus quis eros pharetra feugiat non ac neque. Vivamus lacus eros, feugiat at volutpat at, viverra id nisl. Vivamus ac dolor eleifend libero venenatis pharetra ut iaculis arcu. Donec neque nibh, vehicula non porta a, consectetur eu erat. Sed eleifend, metus vel euismod placerat, lectus lectus sollicitudin nisl, ac elementum sem quam nec dolor. In hac habitasse platea dictumst. Proin vitae suscipit orci. Suspendisse a ipsum vel lorem tempus scelerisque et vitae neque. Proin sodales, tellus sit amet consequat cursus, odio massa ultricies enim, eu fermentum velit lectus in lacus. Quisque eu porttitor diam. Nunc felis purus, facilisis non tristique ac, pulvinar nec nulla. Duis dolor risus, egestas nec tristique ac, ullamcorper cras amet."
       @cxt.eval("'#{lorem}'").should == lorem
     end
-  
+
     it "can pass objects back to ruby" do
       @cxt.eval("({foo: 'bar', baz: 'bang', '5': 5, embedded: {badda: 'bing'}})").tap do |object|
         object.should_not be_nil
@@ -64,7 +64,7 @@ describe "Ruby Javascript API" do
         end
       end
     end
-  
+
     it "unwraps ruby objects returned by embedded ruby code to maintain referential integrity" do
       Object.new.tap do |o|
         @cxt['get'] = lambda {o}
@@ -112,7 +112,7 @@ describe "Ruby Javascript API" do
       @cxt["say"] = lambda {|word, times| word * times}
       @cxt.eval("say('Hello',2)").should == "HelloHello"
     end
-  
+
     it "translates ruby Array to Javascript Array" do
       class_eval do
         def ruby_array
@@ -121,7 +121,7 @@ describe "Ruby Javascript API" do
       end
       evaljs('o.ruby_array instanceof Array').should == true
     end
-  
+
     it "translates ruby Time to Javascript Date" do
       class_eval do
         def ruby_time
@@ -159,7 +159,7 @@ describe "Ruby Javascript API" do
       end
       evaljs('o.say_hello("Gracie")').should == "Hello Gracie!"
     end
-  
+
     it "can call a bound ruby method" do
       five = class_eval do
         def initialize(lhs)
@@ -174,9 +174,9 @@ describe "Ruby Javascript API" do
       @cxt['timesfive'] = five.method(:times)
       @cxt.eval('timesfive(3)').should == 15
     end
-    
+
     describe "Default Ruby Object Access" do
-      
+
       it "can call public locally defined ruby methods" do
         class_eval do
           def voo(str)
@@ -260,7 +260,7 @@ describe "Ruby Javascript API" do
           end
         end
       end
-      
+
       it "allows a ruby object to take a pass on intercepting an indexed property" do
         Class.new.class_eval do
           def initialize
@@ -437,7 +437,7 @@ describe "Ruby Javascript API" do
           evaljs("o[3] = 'three'").should == 'three'
           evaljs("o[3]").should == 'three'
         end
-        
+
         it "doesn't kill the whole process if indexed interceptors throw exceptions" do
           class_eval do
             def [](idx)
@@ -469,7 +469,7 @@ describe "Ruby Javascript API" do
       end
       @cxt.eval("o.whiz('bang')").should == "whizbang!"
     end 
-  
+
     it "treats ruby methods that have an arity of 0 as javascript properties by default" do
       class_eval do
         def property
@@ -492,11 +492,11 @@ describe "Ruby Javascript API" do
       evaljs('o.dollars = 50')
       @instance.dollars.should == 50
     end
-    
+
     it "will accept expando properties by default for properties on ruby object that are not implemented in ruby" do
       evaljs('o.five = 5; o.five').should == 5
     end
-    
+
     it "it silently fails to replace properties which are defined on ruby objects but which are read-only" do
       class_eval do
         def bar
@@ -574,7 +574,7 @@ describe "Ruby Javascript API" do
         def plus(lhs, rhs)
           lhs + rhs
         end
-      
+
         def minus(lhs, rhs)
           lhs - rhs
         end
@@ -666,18 +666,18 @@ describe "Ruby Javascript API" do
         @cxt.eval('new (o.constructor)()')
       }.should raise_error(JSError)
     end
-    
+
     it "extends object to allow for the arbitrary execution of javascript with any object as the scope" do
       Class.new.class_eval do
-      
+
         def initialize
           @lhs = 5
         end
-      
+
         def timesfive(rhs)
           @lhs * rhs     
         end
-      
+
         new.eval_js("timesfive(6)").should == 30
       end
     end
@@ -731,30 +731,30 @@ describe "Ruby Javascript API" do
   end
 
   describe "A Javascript Object Reflected Into Ruby" do
-  
+
     before(:each) do
       @cxt = Context.new
       @o = @cxt.eval("o = new Object(); o")
     end
-  
+
     def evaljs(js)
       @cxt.eval(js)
     end
-  
+
     it "can have its properties manipulated via ruby style [] hash access" do
       @o["foo"] = 'bar'
       evaljs('o.foo').should == "bar"
       evaljs('o.blue = "blam"')
       @o["blue"].should == "blam"
     end
-  
+
     it "doesn't matter if you use a symbol or a string to set a value" do
       @o[:foo] = "bar"
       @o['foo'].should == "bar"
       @o['baz'] = "bang"
       @o[:baz].should == "bang"
     end
-  
+
     it "returns nil when the value is null, null, or not defined" do
       @o[:foo].should be_nil
     end
@@ -778,7 +778,7 @@ EOJS
         h.should == {"foo" => 'bar', "bang" => 'baz', 5 => 'flip'}
       end
     end  
-end
+  end
 
   describe "Exception Handling" do
     it "raises javascript exceptions as ruby exceptions" do
@@ -788,9 +788,9 @@ end
     end
 
     it "can handle syntax errors" do
-    	lambda {
-    		Context.eval('does not compiles')
-    	}.should raise_error
+      lambda {
+        Context.eval('does not compiles')
+      }.should raise_error
     end
 
     it "translates ruby exceptions into javascript exceptions if they are thrown from code called it javascript" do
